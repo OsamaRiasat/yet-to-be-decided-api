e0f84064ab27ca2643c938ede714aee4f978171c
UsamaRiasat1 <usama.riasat@xprolabs.com>
2024-03-18 15:36:32 +0500

    Update(News Letter): Added opt-in attribute in the contact us api

--- company/admin.py
+++ company/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ContactSubmission
+from .models import ContactSubmission, MeetingRequest, NewsletterSubscription
 
 # Register your models here.
 
 admin.site.register(ContactSubmission)
+admin.site.register(MeetingRequest)
+admin.site.register(NewsletterSubscription)
--- company/serializers.py
+++ company/serializers.py
@@ -4,11 +4,19 @@ from .models import ContactSubmission, MeetingRequest, NewsletterSubscription
 
 
 class ContactSubmissionSerializer(serializers.ModelSerializer):
+    opt_in = serializers.BooleanField(required=False)
     class Meta:
         model = ContactSubmission
         fields = '__all__'
 
     def create(self, validated_data):
+        print(validated_data)
+        if validated_data.get('opt_in') is True:
+            # Create a newsletter subscription
+            NewsletterSubscription.objects.create(email=validated_data.get('email'))
+            print('Newsletter subscription created')
+        # remove the opt_in field from the validated data
+        validated_data.pop('opt_in', None)
         return ContactSubmission.objects.create(**validated_data)
 
     def get_region_choices(self):
